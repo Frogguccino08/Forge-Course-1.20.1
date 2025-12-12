a7d577e0526f0f50d2d3903db856d36755d47e63
Frogguccino08 <raul.07@hotmail.com.br>
2025-12-11 23:02:46 -0300

    Concatenação

--- src/main/java/net/raul/mccourse/Blocks/ModBlocks.java
+++ src/main/java/net/raul/mccourse/Blocks/ModBlocks.java
@@ -20,19 +20,18 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, MCCourseMod.MOD_ID);
-
     public static final RegistryObject<Block> SUGILITE_BLOCK = registerBlock("sugilite_block",
             () -> new Block(BlockBehaviour.Properties.of().mapColor(MapColor.METAL).instrument(NoteBlockInstrument.IRON_XYLOPHONE).requiresCorrectToolForDrops().strength(5.0F, 6.0F).sound(SoundType.METAL)));
-public static final RegistryObject<Block> RAW_SUGILITE_BLOCK = registerBlock("raw_sugilite_block",
+    public static final RegistryObject<Block> RAW_SUGILITE_BLOCK = registerBlock("raw_sugilite_block",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.RAW_IRON_BLOCK)));
-public static final RegistryObject<Block> DEEPSLATE_SUGILITE_ORE = registerBlock("deepslate_sugilite_ore",
+    public static final RegistryObject<Block> DEEPSLATE_SUGILITE_ORE = registerBlock("deepslate_sugilite_ore",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.DEEPSLATE_IRON_ORE)));
-public static final RegistryObject<Block> END_STONE_SUGILITE_ORE = registerBlock("end_stone_sugilite_ore",
+    public static final RegistryObject<Block> END_STONE_SUGILITE_ORE = registerBlock("end_stone_sugilite_ore",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.END_STONE)));
-public static final RegistryObject<Block> NETHER_SUGILITE_ORE = registerBlock("nether_sugilite_ore",
+    public static final RegistryObject<Block> NETHER_SUGILITE_ORE = registerBlock("nether_sugilite_ore",
             () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHER_GOLD_ORE)));
-public static final RegistryObject<Block> SUGILITE_ORE = registerBlock("sugilite_ore",
-            () -> new Block(BlockBehaviour.Properties.copy(Blocks.NETHER_GOLD_ORE)));
+    public static final RegistryObject<Block> SUGILITE_ORE = registerBlock("sugilite_ore",
+            () -> new Block(BlockBehaviour.Properties.copy(Blocks.IRON_ORE)));
 
 
 
